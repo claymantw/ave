c16ea420363fa356766cd8063a8b416de4e11aae
saurabh <saurabhdoteth@gmail.com>
2024-07-04 10:48:47 +0530

    chore: rewrite basePath to `/api`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: () => [
+    {
+      source: "/",
+      destination: "/api",
+    },
+  ],
+};
 
 export default nextConfig;
